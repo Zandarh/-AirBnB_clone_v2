2da724bc4ee95356c695358e94aeaa19cb4f2283
Zandarh <ubaksxander@gmail.com>
2022-11-09 14:18:16 +0100

    flask 1

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -5,14 +5,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/", strict_slashes=False)
+
+@app.route("/hbnb", strict_slashes=False)
 def hello():
-    """Says Hello HBNB"""
-    return "Hello HBNB!"
-
-
-@app.route("/", strict_slashes=False)
-def hello_hbnb():
     """Says HBNB"""
     return "HBNB"
 
