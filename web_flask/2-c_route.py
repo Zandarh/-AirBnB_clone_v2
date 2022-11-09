1dbe520ce10c6d32ff388443c1965f2f04451080
Zandarh <ubaksxander@gmail.com>
2022-11-09 14:23:24 +0100

    flask 2

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -5,6 +5,17 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """Displays 'Hello HBNB!'."""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """Displays 'HBNB'."""
+    return "HBNB"
+
 
 @app.route("/c/<text>", strict_slashes=False)
 def hello():
