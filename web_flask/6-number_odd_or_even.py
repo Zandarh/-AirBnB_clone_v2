353e503bc8360c7c54380c2e1e29adc07b0f6e86
Zandarh <ubaksxander@gmail.com>
2022-11-09 15:30:56 +0100

    flask 5

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -6,9 +6,11 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route("/number_odd_or_even/<n>", strict_slashes=False)
-def hello(n):
-    """Says HBNB"""
+@app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
+def number_odd_or_even(n):
+    """Displays an HTML page only if <n> is an integer.
+    States whether <n> is odd or even in the body.
+    """
     return render_template("6-number_odd_or_even.html", n=n)
 
 
